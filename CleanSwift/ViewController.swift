752de8f66d8bcd3ffebe1eee10f8d3c2f3184132
NuntapatHi <nuntapath@gmail.com>
2022-11-07 20:30:22 +0700

    Initial Commit

---
+++ CleanSwift/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CleanSwift
+//
+//  Created by Nuntapat Hirunnattee on 7/11/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
